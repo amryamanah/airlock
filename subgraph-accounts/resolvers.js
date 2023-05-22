46771ac88f28fa79d8c5c288d03bfdd8e3a9113f
Amry Amanah <amry@kitalulus.com>
2023-05-22 22:11:48 +0700

    implement subgraph account until reference resolver section

---
+++ subgraph-accounts/resolvers.js
@@ -0,0 +1,24 @@
+const { AuthenticationError, ForbiddenError } = require('./utils/errors');
+
+const resolvers = {
+  Host: {
+    __resolveReference: (user, { dataSources }) => {
+      console.log(dataSources);
+      return dataSources.accountsAPI.getUser(user.id);
+    },
+  },
+  Guest: {
+    __resolveReference: (user, { dataSources }) => {
+      return dataSources.accountsAPI.getUser(user.id);
+    }
+  },
+  User: {
+    __resolveType(user) {
+      return user.role;
+    },
+  },
+
+  
+};
+
+module.exports = resolvers;
